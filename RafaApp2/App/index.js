004c42d15ceab15641a9e9813f7ac767c86f2663
rg0wv0 <60630406+rg0wv0@users.noreply.github.com>
2021-06-19 19:59:45 -0300

    Home screen set up

--- RafaApp2/App/index.js
+++ RafaApp2/App/index.js
@@ -1,20 +1,4 @@
-/* eslint-disable no-use-before-define */
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import Comousar from './screens/Comousar';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Simples Atos</Text>
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  }
-});
+export default () => <Comousar />;
---
+++ RafaApp2/App/screens/Comousar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  text: {}
+});
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <Text style={styles.text}>Como Usar</Text>
+    </SafeAreaView>
+  );
+};
