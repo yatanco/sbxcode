084355d938009ebacee5b87f9d0f2a5db47b6e1d
rg0wv0 <60630406+rg0wv0@users.noreply.github.com>
2021-06-19 21:00:56 -0300

    Setting up the Como Usar screen

--- RafaApp2/App/screens/Comousar.js
+++ RafaApp2/App/screens/Comousar.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
+
+import colors from '../constants/colors';
 
 const styles = StyleSheet.create({
-  text: {}
+  text: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    fontSize: 26,
+    color: colors.text
+  },
+  separator: {
+    backgroundColor: colors.border,
+    height: StyleSheet.hairlineWidth,
+    marginLeft: 20
+  }
 });
 
 export default () => {
   return (
     <SafeAreaView>
       <Text style={styles.text}>Como Usar</Text>
+
+      <View style={styles.separator} />
+
+      <Text style={styles.text}>Olá</Text>
     </SafeAreaView>
   );
 };
